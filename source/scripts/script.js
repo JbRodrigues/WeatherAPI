4fd0606613634601f03e1c57e6b715dbfef9fbe7
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2023-03-22 19:04:04 -0300

    Add variables and fix the initial screen items position

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,30 +1,51 @@
-
 const apiKey = 'ac4095629fae467398e185840232103';
 
-// Get a reference to the form and its input elements
 const form = document.getElementById("myForm");
 let inputedCity = document.getElementById("inputCity");
-let formCity = document.getElementById("formCity");
-let formRegion = document.getElementById("formRegion");
-let city = inputedCity.value
-// Add an event listener to the form to handle form submission
-form.addEventListener("submit", (event) => {
-    // Prevent the default form submission behavior
-    event.preventDefault();
-    
-    formCity.innerHTML = city;
-});
+let displayCity = document.getElementById("formCity");
+let displayRegion = document.getElementById("formRegion");
+let displayTemp = document.getElementById("formTemp");
+let displayText = document.getElementById("formText");
+let displayIcon = document.getElementById("formIcon");
 
-fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`)
-    .then(response => response.json())
-    .then(data => {
-        let cityName = data.location.name
-        let regionName = data.location.region
-        let currentTemp = data.current.temp_c
-        let currentCondition = data.current.condition.text
-        let currentConditionLogo = data.current.condition.icon
-        formRegion.innerHTML = regionName;
-    })
-    .catch(error => {
-        console.error('Error:', error);
+
+function textSubmit() {
+    // Add an event listener to the form to handle form submission
+    form.addEventListener("submit", (event) => {
+        // Prevent the default form submission behavior
+        event.preventDefault();
+
+        let city = inputedCity.value.toUpperCase();
+        console.log(city)
+        displayCity.innerHTML = city;
     });
+    return city
+}
+
+function fetchFunction(city) {
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`)
+        .then(response => response.json())
+        .then(data => {
+
+            let cityName = data.location.name
+            let regionName = data.location.region
+            let currentTemp = data.current.temp_c
+            let currentCondition = data.current.condition.text
+            let currentConditionLogo = data.current.condition.icon
+            displayCity.innerHTML = cityName;
+            displayRegion.innerHTML = regionName;
+            displayTemp.innerHTML = currentTemp + " &degC";
+            displayIcon.setAttribute("src", currentConditionLogo)
+            displayText.innerHTML = currentCondition;
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+textSubmit(fetchFunction())
+
+
+
