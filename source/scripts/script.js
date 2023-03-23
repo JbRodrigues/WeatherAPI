f95c39a0414789bb3fcd2f087de053e6d25f6f48
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2023-03-23 17:45:01 -0300

    Add background changes when current conditions change

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -3,36 +3,63 @@ const apiKey = 'ac4095629fae467398e185840232103';
 const form = document.getElementById("myForm");
 let inputedCity = document.getElementById("inputCity");
 let displayCity = document.getElementById("formCity");
-let displayRegion = document.getElementById("formRegion");
+let displayCountry = document.getElementById("formCountry");
 let displayTemp = document.getElementById("formTemp");
 let displayText = document.getElementById("formText");
 let displayIcon = document.getElementById("formIcon");
-let city = "Londrina"
 
 
 // Add an event listener to the form to handle form submission
 form.addEventListener("submit", (event) => {
     // Prevent the default form submission behavior
     event.preventDefault();
-    city = inputedCity.value;
-    console.log(inputedCity.value.toUpperCase())
+    let city = inputedCity.value;
 
     fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`)
         .then(response => response.json())
         .then(data => {
-            let cityName = data.location.name
-            let regionName = data.location.region
-            let currentTemp = data.current.temp_c
-            let currentCondition = data.current.condition.text
-            let currentConditionLogo = data.current.condition.icon
+            console.log(data)
+            let regionName = data.location.region;
+            let cityName = data.location.name + " - " + regionName;
+            let countryName = data.location.country;
+            let currentTemp = data.current.temp_c;
+            let currentCondition = data.current.condition.text;
+            let currentConditionLogo = data.current.condition.icon;
+
+            let body = document.querySelector('body');
+            switch (currentCondition) {
+                case "Sunny":
+                    body.className = "sunny"
+
+                    break;
+
+                case "Partly cloudy":
+                    body.className = "partlyCloudly"
+                    break;
+
+                default:
+                    body.className = "default"
+                    break;
+            }
+
+
             displayCity.innerHTML = cityName;
-            displayRegion.innerHTML = regionName;
             displayTemp.innerHTML = currentTemp + " &degC";
-            displayIcon.setAttribute("src", currentConditionLogo)
+            displayIcon.setAttribute("src", currentConditionLogo);
             displayText.innerHTML = currentCondition;
+            displayCountry.innerHTML = countryName;
 
         })
         .catch(error => {
             console.error('Error:', error);
+            let cityName = "City not found ;(";
+            let countryName = "";
+            let currentTemp = "";
+            let currentCondition = "";
+            displayTemp.innerHTML = currentTemp;
+            displayText.innerHTML = currentCondition;
+            displayCountry.innerHTML = countryName;
+            displayCity.innerHTML = cityName;
+            displayIcon.setAttribute("src", 'source/assets/not-found.png');
         });
 });
